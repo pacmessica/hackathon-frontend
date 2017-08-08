2d8faf2a4e98bc4afa6e736af73c6a91c15040df
Jessica Paczuski <jessica@pimmr.com>
2017-08-08 15:31:27 +0200

    Add home link

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Routes from '../routes';
 import styles from './App.scss';
+import {Link} from 'react-router-dom';
+
 
 const App = () =>
     <div className={styles.container}>
+        <Link to={'/'} className={styles.link}>Cases</Link>
         { Routes }
     </div>;
 
