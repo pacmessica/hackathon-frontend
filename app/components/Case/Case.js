254e3f5ee55c7bdecaa62c88fa8880b15807487d
Jessica Paczuski <jessica@pimmr.com>
2017-08-09 14:03:14 +0200

    Change listing page

--- app/components/Case/Case.js
+++ app/components/Case/Case.js
@@ -2,19 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Case.scss';
 import { Post } from './Post';
-import { Filters } from './Filters';
+// import { Filters } from './Filters';
+
+// <p>Set minimal content{"'"}s suspicious score</p>
+// <div className={styles.score}slider">
+//     <input type="range">
+// </div>
+
 
 export const Case = ({ caseQuery, posts, filterCase }) => {
+    console.log({caseQuery, posts, filterCase });
     return (
         <div className={styles.container}>
-            <h1>Search Results</h1>
-            <div className={styles.content}>
-                <ul>
-                    {posts.map((post) => <Post key={post.id} {...post} /> )}
-                </ul>
-                <Filters filterCase={filterCase} caseQuery={caseQuery} />
+            <h1>Suspicious Advertisements</h1>
+            <div className={styles.header}>
+             <p>Filter on...</p>
+             <table className={styles.filters}>
+                 <tbody>
+                     <tr>
+                         <td className={styles.score}><input type="text" placeholder="Description" /></td>
+                         <td className={styles.score}><input type="text" placeholder="Location" /></td>
+                     </tr>
+                 </tbody>
+             </table>
             </div>
-        </div>
+            {posts.map((post) => <Post key={post.id} {...post} onClick={() => console.log('clicked!')}/> )}
+         </div>
+
     );
 };
 
--- app/components/Case/Post.js
+++ app/components/Case/Post.js
@@ -1,26 +1,108 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Post.scss';
+import styles from './Case.scss';
 import htmlToText from 'app/common/parser';
 
-export const Post = ({ title, photos, description, city, created, price }) => {
-    const src = photos[0];
-    const descriptionText = htmlToText(description);
-    return (
-        <li className={styles.item}>
-            <div className={styles.textbox}>
-                <div className={styles.title}>{title}</div>
-                <div className={styles.description}>{descriptionText}</div>
-                <div className={styles.details}>
-                    <div>€ {price}</div>
-                    <div>{created}</div>
-                    <div>{city}</div>
+
+export class Post extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            hidden: true,
+        };
+    }
+
+    toggleHidden = () => {
+        const {hidden} = this.state;
+        this.setState({hidden: !hidden});
+    }
+
+    renderMatch = ({name, score}) => {
+        if (score === 0) {
+            return null;
+        }
+
+        let rule = '';
+        if (name === 'uses_nontrackable_payment') {
+            rule = 'Non-trackable payment';
+        }
+        else if (name === 'multiple_advertisements_single_day') {
+            rule = 'Multiple advertisements in one day';
+        }
+        else if (name === 'user_recently_created') {
+            rule = 'New user';
+        }
+        else if (name === 'is_different_user_same_phone') {
+            rule = 'Different user has same phone';
+        }
+        else if (name === 'is_address_abroad') {
+            rule = 'International address';
+        }
+        else if (name === 'is_high_activity') {
+            rule = 'High user activity';
+        }
+        else if (name === 'very_high_price') {
+            rule = 'Listing has high price';
+        }
+        else if (name === 'is_company') {
+            rule = 'Company';
+        }
+        else if (name === 'is_malafide') {
+            rule = 'User is malafide';
+        }
+        return (
+            <tr>
+                <td>{rule}</td>
+                <td>{score}%</td>
+            </tr>
+        );
+    }
+
+    render() {
+        const {title, photos, description, city, price, suspiscious_score, user, matches} = this.props;
+        const src = photos[0];
+        const descriptionText = htmlToText(description);
+        return (
+            <div id="advertisement-id1" className={styles.boxEffectShowmore} onClick={this.toggleHidden}>
+                <table className={styles.advertisement}>
+                    <tbody>
+                        <tr>
+                            <td className={styles.score}><span>{suspiscious_score}%</span></td>
+                            <td className={styles.title}>{title}</td>
+                            <td><img src={src} width="100" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div id="advertisement-details-id1" className={this.state.hidden ? styles.hidden : ''}>
+                    <h3>What triggered this alert?</h3>
+                    <table className={styles.rules}>
+                        <tbody>
+                            {matches.map(({name, score}) => {
+                                return this.renderMatch({name, score});
+                            })}
+                        </tbody>
+                    </table>
+
+                    <h3>More about this advertisement</h3>
+                    <div className={styles.advertisementDetails}>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>{user.name}</td>
+                                    <td>&euro; {price}</td>
+                                    <td>{city}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div className={styles.description}>{descriptionText}
+                        </div>
+                    </div>
+                    <div className={styles.breaker}>&nbsp;</div>
                 </div>
             </div>
-            <div className={styles.photo} style={{backgroundImage: `url(${src})`}} />
-        </li>
-    );
-};
+        );
+    }
+}
 
 Post.propTypes = {
     title: PropTypes.string.isRequired,
